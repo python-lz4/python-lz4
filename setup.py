f92fd222ff41ad4f37eab0d9d35bd3a149d362b8
Steeve Morin <steeve.morin@gmail.com>
2012-02-29 13:29:21 +0100

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name='lz4',
-    version='0.2',
+    version='0.2.1',
     description=open('README.rst', 'r').read(),
     author='Steeve Morin',
     author_email='steeve.morin@gmail.com',
