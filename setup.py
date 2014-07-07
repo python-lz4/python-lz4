87ac35f0179e3979d9a09b0b3118917d3f8b231b
Steeve Morin <steeve.morin@gmail.com>
2014-07-07 14:41:35 +0200

    Update CFLAGS for build Closes #8, closes #18

--- setup.py
+++ setup.py
@@ -20,7 +20,14 @@ setup(
             'src/lz4.c',
             'src/lz4hc.c',
             'src/python-lz4.c'
-        ], extra_compile_args=["-O4"])
+        ], extra_compile_args=[
+            "-std=c99",
+            "-O3",
+            "-Wall",
+            "-W",
+            "-Wundef",
+            "-DLZ4_VERSION=\"r119\"",
+        ])
     ],
     setup_requires=["nose>=1.0"],
     test_suite = "nose.collector",
