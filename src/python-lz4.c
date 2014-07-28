b6a96a17c130ddce9f99fb0cc4443377b856c7f2
Yue Du <ifduyue@gmail.com>
2014-07-28 23:33:31 +0800

    Also add __version__.
    Ref #19

--- src/python-lz4.c
+++ src/python-lz4.c
@@ -203,6 +203,7 @@ void initlz4(void)
     }
 
     PyModule_AddStringConstant(module, "VERSION", VERSION);
+    PyModule_AddStringConstant(module, "__version__", VERSION);
     PyModule_AddStringConstant(module, "LZ4_VERSION", LZ4_VERSION);
 
 #if PY_MAJOR_VERSION >= 3
