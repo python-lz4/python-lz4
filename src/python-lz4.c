b83960982d1010abd7ece1f02991729f79472dca
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-05-18 22:21:51 +0100

    Remove unused typedef macro code

--- src/python-lz4.c
+++ src/python-lz4.c
@@ -36,24 +36,6 @@
 #include "lz4hc.h"
 #include "python-lz4.h"
 
-
-#if defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)   /* C99 */
-# include <stdint.h>
-  typedef  uint8_t BYTE;
-  typedef uint16_t U16;
-  typedef uint32_t U32;
-  typedef  int32_t S32;
-  typedef uint64_t U64;
-#else
-  typedef unsigned char       BYTE;
-  typedef unsigned short      U16;
-  typedef unsigned int        U32;
-  typedef   signed int        S32;
-  typedef unsigned long long  U64;
-#endif
-
-
-
 #define MAX(a, b)               ((a) > (b) ? (a) : (b))
 
 typedef int (*compressor)(const char *source, char *dest, int isize);
