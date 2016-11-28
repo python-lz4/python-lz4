0cfc4c80554446a59ac015d7fb7fc1e53c7972d7
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-11-28 14:19:05 +0000

    Add tests for autoFlush=0 case

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1,5 +1,6 @@
 import lz4.frame as lz4frame
 import unittest
+import os
 
 class TestLZ4Frame(unittest.TestCase):
     def test_create_and_free_compression_context(self):
@@ -28,6 +29,44 @@ class TestLZ4Frame(unittest.TestCase):
         decompressed = lz4frame.decompress(compressed,1024)
         self.assertEqual(input_data, decompressed)
 
+    def test_compress_begin_update_end_no_auto_flush(self):
+        context = lz4frame.create_compression_context()
+        self.assertNotEqual(context, None)
+
+        input_data = b"2099023098234882923049823094823094898239230982349081231290381209380981203981209381238901283098908123109238098123"
+        chunk_size = int((len(input_data)/2)+1)
+        compressed = lz4frame.compress_begin(context, auto_flush=0)
+        compressed += lz4frame.compress_update(context, input_data[:chunk_size])
+        compressed += lz4frame.compress_update(context, input_data[chunk_size:])
+        compressed += lz4frame.compress_end(context)
+        lz4frame.free_compression_context(context)
+
+        decompressed = lz4frame.decompress(compressed,1024)
+        self.assertEqual(input_data, decompressed)
+
+    def test_compress_begin_update_end_no_auto_flush_2(self):
+        input_data = os.urandom(4 * 128 * 1024)  # Read 4 * 128kb
+
+        context = lz4frame.create_compression_context()
+        self.assertNotEqual(context, None)
+
+        compressed = lz4frame.compress_begin(context, auto_flush=0)
+
+        chunk_size = 32 * 1024 # 32 kb, half of default block size
+        start = 0
+        end = start + chunk_size
+        while start <= len(input_data):
+            print 'hello'
+            print len(input_data[start:end])
+            compressed += lz4frame.compress_update(context, input_data[start:end])
+            start = end
+            end = start + chunk_size
+
+        compressed += lz4frame.compress_end(context)
+        lz4frame.free_compression_context(context)
+
+        decompressed = lz4frame.decompress(compressed, len(input_data))
+        self.assertEqual(input_data, decompressed)
 
     def test_get_frame_info(self):
         input_data = b"2099023098234882923049823094823094898239230982349081231290381209380981203981209381238901283098908123109238098123"
