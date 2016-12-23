python-lz4/python-lz4

commit 54fe1ed7c010f542df03b1af30d4f4e7e3a694cb

Fix raw memory disclosure in lz4.decompress
python -c 'import lz4; print repr(lz4.decompress(lz4.compress("A" * 64, store_size=False), uncompressed_size=79))'

tests/test_block.py
@@ -170,6 +170,31 @@ class TestLZ4Block(unittest.TestCase):
         assert data == out
         pool.close()
 
+    def test_decompress_ui32_overflow(self):
+        data = lz4.compress(b'A' * 64)
+        self.assertEqual(data[:4], b'\x40\0\0\0')
+        self.assertEqual(lz4.decompress(data[4:], uncompressed_size=64), b'A' * 64)
+        with self.assertRaisesRegexp(OverflowError, r'^signed integer is greater than maximum$'):
+            lz4.decompress(data[4:], uncompressed_size=((1<<32) + 64))
+
+    def test_decompress_without_leak(self):
+        # Verify that hand-crafted packet does not leak uninitialized(?) memory.
+        data = lz4.compress(b'A' * 64)
+        with self.assertRaisesRegexp(ValueError, r'^Decompressor writes 64 bytes, but 79 are claimed$'):
+            lz4.decompress(b'\x4f' + data[1:])
+        with self.assertRaisesRegexp(ValueError, r'^Decompressor writes 64 bytes, but 79 are claimed$'):
+            lz4.decompress(data[4:], uncompressed_size=79)
+
+    def test_decompress_with_trailer(self):
+        data = b'A' * 64
+        comp = lz4.compress(data)
+        with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
+            self.assertEqual(data, lz4.block.decompress(comp + 'A'))
+        with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
+            self.assertEqual(data, lz4.block.decompress(comp + comp))
+        with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
+            self.assertEqual(data, lz4.block.decompress(comp + comp[4:]))
+
 if __name__ == '__main__':
     unittest.main()
 

lz4/block/_block.c
@@ -283,6 +283,12 @@ decompress (PyObject * Py_UNUSED (self), PyObject * args, PyObject * kwargs)
       PyErr_Format (PyExc_ValueError, "Corrupt input at byte %d", -output_size);
       Py_CLEAR (py_dest);
     }
+  else if ((size_t)output_size != dest_size)
+    {
+      // IMHO, it's better to fail explicitly than to allow fishy data to pass through.
+      PyErr_Format (PyExc_ValueError, "Decompressor writes %d bytes, but %zu are claimed", output_size, dest_size);
+      Py_CLEAR (py_dest);
+    }
 
   return py_dest;
 }

commit 41acc8ed5c7db24c9d71ea0a4f95279d413f20f5

Fix python 2.6 and 3.x compatibility broken in 54fe1ed7

tests/test_block.py
@@ -170,10 +170,14 @@ class TestLZ4Block(unittest.TestCase):
         assert data == out
         pool.close()
 
-    def test_decompress_ui32_overflow(self):
+    def test_block_format(self):
         data = lz4.compress(b'A' * 64)
         self.assertEqual(data[:4], b'\x40\0\0\0')
         self.assertEqual(lz4.decompress(data[4:], uncompressed_size=64), b'A' * 64)
+
+class TestLZ4BlockModern(unittest.TestCase):
+    def test_decompress_ui32_overflow(self):
+        data = lz4.compress(b'A' * 64)
         with self.assertRaisesRegexp(OverflowError, r'^signed integer is greater than maximum$'):
             lz4.decompress(data[4:], uncompressed_size=((1<<32) + 64))
 
@@ -189,11 +193,15 @@ class TestLZ4Block(unittest.TestCase):
         data = b'A' * 64
         comp = lz4.compress(data)
         with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
-            self.assertEqual(data, lz4.block.decompress(comp + 'A'))
+            self.assertEqual(data, lz4.block.decompress(comp + b'A'))
         with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
             self.assertEqual(data, lz4.block.decompress(comp + comp))
         with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
             self.assertEqual(data, lz4.block.decompress(comp + comp[4:]))
+
+if sys.version_info < (2, 7):
+    # Poor-man unittest.TestCase.skip for Python 2.6
+    del TestLZ4BlockModern
 
 if __name__ == '__main__':
     unittest.main()