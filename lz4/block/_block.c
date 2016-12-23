c90f487ae97304f40cf5a232aeaefa18aa0138c4
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-12-23 20:12:19 +0000

    Move Visual Studio handling to top of _block.c
    VS doesn't have inline, only __inline. So, we have to define inline
    ahead of including the py3c headers (which use inline).

--- lz4/block/_block.c
+++ lz4/block/_block.c
@@ -28,6 +28,20 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#if defined(_WIN32) && defined(_MSC_VER)
+#define inline __inline
+#if _MSC_VER >= 1600
+#include <stdint.h>
+#else /* _MSC_VER >= 1600 */
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+#endif /* _MSC_VER >= 1600 */
+#endif
+
 #include <py3c.h>
 #include <py3c/capsulethunk.h>
 
@@ -42,20 +56,6 @@
 #else
 #define Py_UNUSED(name) _unused_ ## name
 #endif
-#endif
-
-#if defined(_WIN32) && defined(_MSC_VER)
-#define inline __inline
-#if _MSC_VER >= 1600
-#include <stdint.h>
-#else /* _MSC_VER >= 1600 */
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-#endif /* _MSC_VER >= 1600 */
 #endif
 
 #if defined(__SUNPRO_C) || defined(__hpux) || defined(_AIX)
