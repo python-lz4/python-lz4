eb46cc86eb22cb916520ff6a25bb4271e6e5030e
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-12-24 00:05:42 +0000

    Remove unused typedefs and move inline definitions to top of _frame.c

--- lz4/frame/_frame.c
+++ lz4/frame/_frame.c
@@ -29,6 +29,12 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  */
+#if defined(_WIN32) && defined(_MSC_VER)
+#define inline __inline
+#elif defined(__SUNPRO_C) || defined(__hpux) || defined(_AIX)
+#define inline
+#endif
+
 #include <py3c.h>
 #include <py3c/capsulethunk.h>
 
@@ -41,25 +47,6 @@
 #else
 #define Py_UNUSED(name) _unused_ ## name
 #endif
-#endif
-
-
-#if defined(_WIN32) && defined(_MSC_VER)
-#define inline __inline
-#if _MSC_VER >= 1600
-#include <stdint.h>
-#else /* _MSC_VER >= 1600 */
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-#endif /* _MSC_VER >= 1600 */
-#endif
-
-#if defined(__SUNPRO_C) || defined(__hpux) || defined(_AIX)
-#define inline
 #endif
 
 static const char * capsule_name = "_frame.LZ4F_cctx";
