f87c1bf452459371280f79b009080eef39c64a9b
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-12-24 00:15:42 +0000

    Re-organize pre-processor defines in _block.c

--- lz4/block/_block.c
+++ lz4/block/_block.c
@@ -28,18 +28,11 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
 #if defined(_WIN32) && defined(_MSC_VER)
 #define inline __inline
-#if _MSC_VER >= 1600
-#include <stdint.h>
-#else /* _MSC_VER >= 1600 */
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-#endif /* _MSC_VER >= 1600 */
+#elif defined(__SUNPRO_C) || defined(__hpux) || defined(_AIX)
+#define inline
 #endif
 
 #include <py3c.h>
@@ -58,8 +51,17 @@ typedef unsigned int uint32_t;
 #endif
 #endif
 
-#if defined(__SUNPRO_C) || defined(__hpux) || defined(_AIX)
-#define inline
+#if defined(_WIN32) && defined(_MSC_VER)
+#if _MSC_VER >= 1600
+#include <stdint.h>
+#else /* _MSC_VER >= 1600 */
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+#endif /* _MSC_VER >= 1600 */
 #endif
 
 static inline void
