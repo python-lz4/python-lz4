1fc99d9e966849e4439ad00669ce10d426431bc5
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-12-29 17:25:12 +0000

    Move deprecated defs to top level and use a deprecated decorator

--- lz4/__init__.py
+++ lz4/__init__.py
@@ -9,6 +9,47 @@ except ImportError:
 
 from ._version import lz4version
 
-# Import these at top level for backwards compatibility, but will be
-# removed in the future
-from .block import *
+# The following definitions are for backwards compatibility, and will
+# be removed in the future
+import lz4.block
+from .deprecated import deprecated
+
+@deprecated('use lz4.block.compress instead')
+def compress(*args, **kwargs):
+    return lz4.block.compress(*args, **kwargs)
+
+@deprecated('use lz4.block.decompress instead')
+def decompress(*args, **kwargs):
+    return lz4.block.decompress(*args, **kwargs)
+
+@deprecated('use lz4.block.compress instead')
+def LZ4_compress(source):
+    return lz4.block.compress(source)
+
+@deprecated('use lz4.block.compress instead')
+def dumps(source):
+    return lz4.block.compress(source)
+
+@deprecated('use lz4.block.compress instead')
+def LZ4_compress_fast(source):
+    return lz4.block.compress(source, mode='fast')
+
+@deprecated('use lz4.block.compress instead')
+def compress_fast(source):
+    return lz4.block.compress(source, mode='fast')
+
+@deprecated('use lz4.block.compress instead')
+def compressHC(source):
+    return lz4.block.compress(source, mode='high_compression')
+
+@deprecated('use lz4.block.decompress instead')
+def uncompress(source):
+    return lz4.block.decompress(source)
+
+@deprecated('use lz4.block.decompress instead')
+def LZ4_uncompress(source):
+    return lz4.block.decompress(source)
+
+@deprecated('use lz4.block.decompress instead')
+def loads(source):
+    return lz4.block.decompress(source)
---
+++ lz4/deprecated.py
@@ -0,0 +1,41 @@
+# See:
+# http://stackoverflow.com/questions/2536307/decorators-in-the-python-standard-lib-deprecated-specifically
+
+import functools
+import inspect
+import warnings
+
+class deprecated(object):
+    def __init__(self, reason):
+        if inspect.isclass(reason) or inspect.isfunction(reason):
+            raise TypeError("Reason for deprecation must be supplied")
+        self.reason = reason
+
+    def __call__(self, cls_or_func):
+        if inspect.isfunction(cls_or_func):
+            if hasattr(cls_or_func, 'func_code'):
+                _code = cls_or_func.func_code
+            else:
+                _code = cls_or_func.__code__
+            fmt = "Call to deprecated function or method {name} ({reason})."
+            filename = _code.co_filename
+            lineno = _code.co_firstlineno + 1
+
+        elif inspect.isclass(cls_or_func):
+            fmt = "Call to deprecated class {name} ({reason})."
+            filename = cls_or_func.__module__
+            lineno = 1
+
+        else:
+            raise TypeError(type(cls_or_func))
+
+        msg = fmt.format(name=cls_or_func.__name__, reason=self.reason)
+
+        @functools.wraps(cls_or_func)
+        def new_func(*args, **kwargs):
+            warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+            warnings.warn_explicit(msg, category=DeprecationWarning, filename=filename, lineno=lineno)
+            warnings.simplefilter('default', DeprecationWarning)  # reset filter
+            return cls_or_func(*args, **kwargs)
+
+        return new_func
