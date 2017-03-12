1b592cf3b24c6fceab9f774c80786be1bc1563dd
Jonathan Underwood <jonathan.underwood@gmail.com>
2017-03-12 15:30:59 +0000

    Really remove deprecation warnings from tests

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,4 +1,4 @@
-import lz4.block as lz4
+import lz4.block
 import sys
 
 
@@ -10,44 +10,44 @@ class TestLZ4Block(unittest.TestCase):
 
     def test_random(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
-      self.assertEqual(DATA, lz4.block.decompress(lz4.compress(DATA)))
+      self.assertEqual(DATA, lz4.block.decompress(lz4.block.compress(DATA)))
 
     def test_random_hc1(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
       self.assertEqual(DATA, lz4.block.decompress(
-          lz4.compress(DATA, mode='high_compression')))
+          lz4.block.compress(DATA, mode='high_compression')))
 
     def test_random_hc2(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
       self.assertEqual(DATA, lz4.block.decompress(
-          lz4.compress(DATA, mode='high_compression', compression=7)))
+          lz4.block.compress(DATA, mode='high_compression', compression=7)))
 
     def test_random_hc3(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
       self.assertEqual(DATA, lz4.block.decompress(
-          lz4.compress(DATA, mode='high_compression', compression=16)))
+          lz4.block.compress(DATA, mode='high_compression', compression=16)))
 
     def test_random_fast1(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
       self.assertEqual(DATA, lz4.block.decompress(
-          lz4.compress(DATA, mode='fast')))
+          lz4.block.compress(DATA, mode='fast')))
 
     def test_random_fast2(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
       self.assertEqual(DATA, lz4.block.decompress(
-          lz4.compress(DATA, mode='fast', acceleration=5)))
+          lz4.block.compress(DATA, mode='fast', acceleration=5)))
 
     def test_random_fast3(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
       self.assertEqual(DATA, lz4.block.decompress(
-          lz4.compress(DATA, mode='fast', acceleration=9)))
+          lz4.block.compress(DATA, mode='fast', acceleration=9)))
 
     def test_random_no_store_size(self):
       DATA = os.urandom(128 * 1024)  # Read 128kb
 
       self.assertEqual(
           DATA, lz4.block.decompress(
-              lz4.compress(DATA, store_size=False),
+              lz4.block.compress(DATA, store_size=False),
               uncompressed_size=128 * 1024
           )
       )
@@ -57,7 +57,7 @@ class TestLZ4Block(unittest.TestCase):
 
       self.assertEqual(
           DATA, lz4.block.decompress(
-              lz4.compress(DATA, mode='high_compression', compression=9, store_size=False),
+              lz4.block.compress(DATA, mode='high_compression', compression=9, store_size=False),
               uncompressed_size=128 * 1024
           )
       )
@@ -67,7 +67,7 @@ class TestLZ4Block(unittest.TestCase):
 
       self.assertEqual(
           DATA, lz4.block.decompress(
-              lz4.compress(DATA, mode='fast', acceleration=9, store_size=False),
+              lz4.block.compress(DATA, mode='fast', acceleration=9, store_size=False),
               uncompressed_size=128 * 1024
           )
       )
@@ -75,7 +75,7 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtrip(x):
-            return lz4.decompress(lz4.compress(x))
+            return lz4.block.decompress(lz4.block.compress(x))
 
         pool = ThreadPool(8)
         out = pool.map(roundtrip, data)
@@ -85,7 +85,7 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_hc1(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtriphc(x):
-            return lz4.decompress(lz4.compress(x, mode='high_compression'))
+            return lz4.block.decompress(lz4.block.compress(x, mode='high_compression'))
 
         pool = ThreadPool(8)
         out = pool.map(roundtriphc, data)
@@ -95,8 +95,8 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_hc2(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtriphc(x):
-            return lz4.decompress(
-                lz4.compress(x, mode='high_compression', compression=4))
+            return lz4.block.decompress(
+                lz4.block.compress(x, mode='high_compression', compression=4))
 
         pool = ThreadPool(8)
         out = pool.map(roundtriphc, data)
@@ -106,8 +106,8 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_hc3(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtriphc(x):
-            return lz4.decompress(
-                lz4.compress(x, mode='high_compression', compression=9))
+            return lz4.block.decompress(
+                lz4.block.compress(x, mode='high_compression', compression=9))
 
         pool = ThreadPool(8)
         out = pool.map(roundtriphc, data)
@@ -117,7 +117,7 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_fast1(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtripfast(x):
-            return lz4.decompress(lz4.compress(x, mode='fast', acceleration=1))
+            return lz4.block.decompress(lz4.block.compress(x, mode='fast', acceleration=1))
 
         pool = ThreadPool(8)
         out = pool.map(roundtripfast, data)
@@ -127,7 +127,7 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_fast2(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtripfast(x):
-            return lz4.decompress(lz4.compress(x, mode='fast', acceleration=4))
+            return lz4.block.decompress(lz4.block.compress(x, mode='fast', acceleration=4))
 
         pool = ThreadPool(8)
         out = pool.map(roundtripfast, data)
@@ -137,7 +137,7 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_fast3(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtripfast(x):
-            return lz4.decompress(lz4.compress(x, mode='fast', acceleration=8))
+            return lz4.block.decompress(lz4.block.compress(x, mode='fast', acceleration=8))
 
         pool = ThreadPool(8)
         out = pool.map(roundtripfast, data)
@@ -147,8 +147,8 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_fast_no_store_size(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtripfast(x):
-            return lz4.decompress(
-                lz4.compress(x, mode='fast', acceleration=8, store_size=False),
+            return lz4.block.decompress(
+                lz4.block.compress(x, mode='fast', acceleration=8, store_size=False),
                 uncompressed_size=128 * 1024
             )
 
@@ -160,8 +160,8 @@ class TestLZ4Block(unittest.TestCase):
     def test_threads_hc_no_store_size(self):
         data = [os.urandom(128 * 1024) for i in range(100)]
         def roundtriphc(x):
-            return lz4.decompress(
-                lz4.compress(x, mode='high_compression', store_size=False, compression=4),
+            return lz4.block.decompress(
+                lz4.block.compress(x, mode='high_compression', store_size=False, compression=4),
                 uncompressed_size=128 * 1024
             )
 
@@ -171,34 +171,34 @@ class TestLZ4Block(unittest.TestCase):
         pool.close()
 
     def test_block_format(self):
-        data = lz4.compress(b'A' * 64)
+        data = lz4.block.compress(b'A' * 64)
         self.assertEqual(data[:4], b'\x40\0\0\0')
-        self.assertEqual(lz4.decompress(data[4:], uncompressed_size=64), b'A' * 64)
+        self.assertEqual(lz4.block.decompress(data[4:], uncompressed_size=64), b'A' * 64)
 
 class TestLZ4BlockModern(unittest.TestCase):
     def test_decompress_ui32_overflow(self):
-        data = lz4.compress(b'A' * 64)
+        data = lz4.block.compress(b'A' * 64)
         with self.assertRaises(OverflowError):
-            lz4.decompress(data[4:], uncompressed_size=((1<<32) + 64))
+            lz4.block.decompress(data[4:], uncompressed_size=((1<<32) + 64))
 
     def test_decompress_without_leak(self):
         # Verify that hand-crafted packet does not leak uninitialized(?) memory.
-        data = lz4.compress(b'A' * 64)
+        data = lz4.block.compress(b'A' * 64)
         with self.assertRaisesRegexp(ValueError, r'^Decompressor wrote 64 bytes, but 79 bytes expected from header$'):
-            lz4.decompress(b'\x4f' + data[1:])
+            lz4.block.decompress(b'\x4f' + data[1:])
         with self.assertRaisesRegexp(ValueError, r'^Decompressor wrote 64 bytes, but 79 bytes expected from header$'):
-            lz4.decompress(data[4:], uncompressed_size=79)
+            lz4.block.decompress(data[4:], uncompressed_size=79)
 
     def test_decompress_truncated(self):
         input_data = b"2099023098234882923049823094823094898239230982349081231290381209380981203981209381238901283098908123109238098123" * 24
-        compressed = lz4.compress(input_data)
+        compressed = lz4.block.compress(input_data)
         for i in range(len(compressed)):
             with self.assertRaisesRegexp(ValueError, '^(Input source data size too small|Corrupt input at byte \d+|Decompressor wrote \d+ bytes, but \d+ bytes expected from header)'):
-                lz4.decompress(compressed[:i])
+                lz4.block.decompress(compressed[:i])
 
     def test_decompress_with_trailer(self):
         data = b'A' * 64
-        comp = lz4.compress(data)
+        comp = lz4.block.compress(data)
         with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
             self.assertEqual(data, lz4.block.decompress(comp + b'A'))
         with self.assertRaisesRegexp(ValueError, r'^Corrupt input at byte'):
