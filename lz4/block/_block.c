feb2a00e3367d1313b795b40442ca3bdfda1abf2
Jonathan Underwood <jonathan.underwood@gmail.com>
2017-04-28 10:47:58 +0100

    Indentation fix in _block.c

--- lz4/block/_block.c
+++ lz4/block/_block.c
@@ -188,7 +188,7 @@ compress (PyObject * Py_UNUSED (self), PyObject * args, PyObject * kwargs)
       break;
     case HIGH_COMPRESSION:
       output_size = LZ4_compress_HC (source, dest_start, source_size,
-                                       dest_size, compression);
+                                     dest_size, compression);
       break;
     }
 
