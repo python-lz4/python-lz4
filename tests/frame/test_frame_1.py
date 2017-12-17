f368000330504b463e79d1baadc6deffb95a7cad
Jonathan Underwood <jonathan.underwood@gmail.com>
2017-12-17 20:43:42 +0000

    Move get_frame_info testing to new file

--- tests/frame/test_frame_1.py
+++ tests/frame/test_frame_1.py
@@ -20,32 +20,6 @@ def test_roundtrip_1(data, block_size, block_mode,
         block_mode=block_mode,
         content_checksum=content_checksum,
     )
-
-    frame_info = lz4frame.get_frame_info(compressed)
-
-    assert frame_info["content_checksum"] == content_checksum
-
-    assert frame_info["skippable"] == False
-
-    if store_size is True:
-        assert frame_info["content_size"] == len(data)
-    else:
-        assert frame_info["content_size"] == 0
-
-    if len(data) > frame_info['block_size'] and block_mode == lz4frame.BLOCKMODE_LINKED:
-        if block_mode ==lz4frame.BLOCKMODE_LINKED:
-            assert frame_info["blocks_linked"] == True
-        else:
-            assert frame_info["blocks_linked"] == False
-        if block_size == lz4frame.BLOCKSIZE_DEFAULT:
-            assert frame_info["block_size_id"] == lz4frame.BLOCKSIZE_MAX64KB
-        else:
-            assert frame_info["block_size_id"] == block_size
-    else:
-        # If there's only a single block in the frame, then LZ4 lib will set
-        # the block mode to be independent
-        assert frame_info["blocks_linked"] == False
-
     decompressed, bytes_read = lz4frame.decompress(compressed)
     assert bytes_read == len(compressed)
     assert decompressed == data
