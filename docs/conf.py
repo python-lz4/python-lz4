30d31a5e628d66d1c274c64a429be921c9da174a
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2018-02-04 12:43:27 +0000

    Use sphinx_bootstrap_theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -105,6 +105,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'sphinxdoc'
+import sphinx_bootstrap_theme
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
