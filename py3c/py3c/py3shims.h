c343400d9807e8969a279170f33e65b424bc0795
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2018-02-10 21:36:14 +0000

    Update bundled py3c headers to version 1.0 files

--- py3c/py3c/py3shims.h
+++ py3c/py3c/py3shims.h
@@ -3,7 +3,7 @@
  */
 
 /*
- * Shims for the  PyMem_Raw* functions added inPython 3.3
+ * Shims for new functionality from in Python 3.3+
  *
  * See https://docs.python.org/3/c-api/memory.html#raw-memory-interface
  */
@@ -14,12 +14,27 @@
 #include <stdlib.h>
 
 
+/* Py_UNUSED - added in Python 3.4, documneted in 3.7 */
+
+#ifndef Py_UNUSED
+#ifdef __GNUC__
+#define Py_UNUSED(name) _unused_ ## name __attribute__((unused))
+#else
+#define Py_UNUSED(name) _unused_ ## name
+#endif
+#endif
+
+
+/* PyMem_Raw{Malloc,Realloc,Free} - added in Python 3.4 */
+
 #if PY_MAJOR_VERSION < 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION < 4)
 #define PyMem_RawMalloc(n) malloc((n) || 1)
 #define PyMem_RawRealloc(p, n) realloc(p, (n) || 1)
 #define PyMem_RawFree(p) free(p)
 #endif /* version < 3.4 */
 
+
+/* PyMem_RawCalloc - added in Python 3.5 */
 
 #if PY_MAJOR_VERSION < 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION < 5)
 #define PyMem_RawCalloc(n, s) calloc((n) || 1, (s) || 1)
