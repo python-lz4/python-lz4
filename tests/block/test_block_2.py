a94b036943cd2ee52b2fbdcd3bda87069bc76298
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2018-06-02 15:48:08 +0100

    Remove lz4 imports from test_huge for debugging

--- tests/block/test_block_2.py
+++ tests/block/test_block_2.py
@@ -1,6 +1,6 @@
 import pytest
 import sys
-import lz4.block
+#import lz4.block
 
 @pytest.mark.skipif(sys.maxsize < 0xffffffff,
                     reason='Py_ssize_t too small for this test')
@@ -10,22 +10,22 @@ def test_huge():
     except MemoryError:
         pytest.skip('Insufficient system memory for this test')
 
-    with pytest.raises(
-            OverflowError, match='Input too large for LZ4 API'
-    ):
-        lz4.block.compress(huge)
+    # with pytest.raises(
+    #         OverflowError, match='Input too large for LZ4 API'
+    # ):
+    #     lz4.block.compress(huge)
 
-    with pytest.raises(
-            OverflowError, match='Dictionary too large for LZ4 API'
-    ):
-        lz4.block.compress(b'', dict=huge)
+    # with pytest.raises(
+    #         OverflowError, match='Dictionary too large for LZ4 API'
+    # ):
+    #     lz4.block.compress(b'', dict=huge)
 
-    with pytest.raises(
-            OverflowError, match='Input too large for LZ4 API'
-    ):
-        lz4.block.decompress(huge)
+    # with pytest.raises(
+    #         OverflowError, match='Input too large for LZ4 API'
+    # ):
+    #     lz4.block.decompress(huge)
 
-    with pytest.raises(
-            OverflowError, match='Dictionary too large for LZ4 API'
-    ):
-            lz4.block.decompress(b'', dict=huge)
+    # with pytest.raises(
+    #         OverflowError, match='Dictionary too large for LZ4 API'
+    # ):
+    #         lz4.block.decompress(b'', dict=huge)
