6be649ff8811a66bd6d5502968b9eb3146cdbb31
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2019-11-10 08:37:19 +0000

    Remove support for Python < 3.5 from CICD and setup.py

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ setup(
     use_scm_version={
         'write_to': "lz4/version.py",
     },
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=3.5",
     setup_requires=[
         'setuptools_scm',
         'pkgconfig',
@@ -211,10 +211,9 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: C',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
