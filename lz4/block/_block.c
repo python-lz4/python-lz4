4b26dca06d9d89ff7cac496c85397158d60e2cee
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2019-12-28 19:32:04 +0000

    Remove Py_UNUSED macro from _block.c

--- lz4/block/_block.c
+++ lz4/block/_block.c
@@ -42,14 +42,6 @@
 #include <lz4.h>
 #include <lz4hc.h>
 
-#ifndef Py_UNUSED /* This is already defined for Python 3.4 onwards */
-#ifdef __GNUC__
-#define Py_UNUSED(name) _unused_ ## name __attribute__((unused))
-#else
-#define Py_UNUSED(name) _unused_ ## name
-#endif
-#endif
-
 #if defined(_WIN32) && defined(_MSC_VER)
 #if _MSC_VER >= 1600
 #include <stdint.h>
