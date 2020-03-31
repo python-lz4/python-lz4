bfa2a4fb8f45e1154899b157c20d5712cd07f553
Samuel Martin <s.martin49@gmail.com>
2020-03-31 15:27:03 +0200

    Bug fix: Fix output buffer size calculation
    The store_comp_size field from the context is not yet set.

--- lz4/stream/_stream.c
+++ lz4/stream/_stream.c
@@ -907,7 +907,7 @@ _create_context (PyObject * Py_UNUSED (self), PyObject * args, PyObject * kwds)
   if (context->config.direction == COMPRESS)
     {
       context->output.len = get_compress_bound (buffer_size);
-      total_size = context->output.len + context->config.store_comp_size;
+      total_size = context->output.len + store_comp_size;
 
       if (context->output.len == 0)
         {
